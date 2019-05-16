a81ef938608f4ce4fae2ccd089625ee7a800ebfe
micnekr <44928743+micnekr@users.noreply.github.com>
2019-05-16 17:34:55 +0100

    correct errors

---
+++ functions.py
@@ -0,0 +1,52 @@
+def A():
+  return "A"
+def B():
+  return "B"
+def C ():
+  return "C"
+def D():
+  return "D"
+def E():
+  return "E"
+def F():
+  return "F"
+def G():
+  return "G"
+def H():
+  return "H"
+def I():
+  return "I"
+def J():
+  return "J"
+def K():
+  return "K"
+def L():
+  return "L"
+def M():
+  return "M"
+def N():
+  return "N"
+def O():
+  return "O"
+def P():
+  return "P"
+def Q():
+  return "Q"
+def R():
+  return "R"
+def S():
+  return "S"
+def T():
+  return "T"
+def U():
+  return "U"
+def V():
+  return "V"
+def W():
+  return "W"
+def X():
+  return "X"
+def Y():
+  return "Y"
+def Z():
+  return "Z"
---
+++ main.py
@@ -0,0 +1,20 @@
+from random import randint
+from functions import *
+
+# create a gitnub repository
+
+DNA = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+DNA_search = input("which DNA strand do you want?" + "\n")
+
+DNA_search = int(DNA_search)
+
+print()
+
+print(DNA[DNA_search]())
+
+creature_A = [C,A,T]
+x = 0
+list_length = len(creature_A)
+while x < list_length:
+  print(creature_A[x]())
+  x+=1
